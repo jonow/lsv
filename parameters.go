b90d98a7db689b15167a14d025340f746f5c3f3b
Jono Wenger <jono@elixxir.io>
2022-10-14 14:23:36 -0700

    Add license header

--- parameters.go
+++ parameters.go
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 jonow                                                   //
+//                                                                            //
+// Use of this source code is governed by an MIT-style license that can be    //
+// found in the LICENSE file.                                                 //
+////////////////////////////////////////////////////////////////////////////////
+
 package lsv
 
 import (
